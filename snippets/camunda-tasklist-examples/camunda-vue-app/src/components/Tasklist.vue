f74a4a9762630c27b54c287b820b0fb124b8f790
kurianbenoy <kurian.benoy@aot-technologies.com>
2021-01-27 10:30:30 +0530

    Make changes in semantic tasklist

--- snippets/camunda-tasklist-examples/camunda-vue-app/src/components/Tasklist.vue
+++ snippets/camunda-tasklist-examples/camunda-vue-app/src/components/Tasklist.vue
@@ -2,23 +2,47 @@
   <div>
     <sui-grid>
       <sui-grid-row :columns="2">
-        <sui-grid-column :width="3">
-          <sui-segment raised>
+        <sui-grid-column :width="5">
+          Created
+          <sui-segment >
             <sui-list divided relaxed v-if="tasks && tasks.length">
-              <sui-list-item v-for="task of tasks">
-                <sui-list-icon name="browser" size="large" vertical-align="middle" />
+              <sui-list-item v-for="task of tasks" :key="task">
                 <sui-list-content>
                   <p><strong>
                     <router-link :to="`/tasklist/${task.id}`">{{task.name}}</router-link><br>
+                    <p>My Process</p>
+                    {{task.assignee}}
                     {{task.created}}
+                    <p>Created 1 hour ago 50</p>
                   </strong></p>
                 </sui-list-content>
               </sui-list-item>
             </sui-list>
           </sui-segment>
         </sui-grid-column>
-        <sui-grid-column :width="13">
+        <sui-grid-column :width="11">
           <sui-segment raised>
+            <h2> Approve Data</h2>
+            <h2> MyData</h2>
+            
+            <sui-list-item>
+              <sui-list-icon name="calendar alternate" />
+              <sui-list-content> Set follow-up date</sui-list-content>
+            </sui-list-item>
+            <sui-list-item>
+              <sui-list-icon name="bell" />
+              <sui-list-content> Due date</sui-list-content>
+            </sui-list-item>
+            <sui-list-item icon="th" content="Add groups"></sui-list-item>
+            <sui-list-item icon="user" content="Claim"></sui-list-item>
+
+            <sui-menu :widths="4">
+            <sui-menu-item>Form</sui-menu-item>
+            <sui-menu-item>History</sui-menu-item>
+            <sui-menu-item active>Diagrams</sui-menu-item>
+            <sui-menu-item>Description</sui-menu-item>
+            </sui-menu>
+            
             <generic-form v-if="this.$route.params.taskId" :taskId="this.$route.params.taskId" :formKey="taskFormKey"></generic-form>
             <div v-if="!this.$route.params.taskId">
               <p>Please choose task.</p>
@@ -68,3 +92,9 @@
   };
 
 </script>
+
+<style>
+  #ul_top_hypers li {
+    display: inline;
+}
+</style>
